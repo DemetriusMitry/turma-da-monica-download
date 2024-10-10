7d42066b85f3c62b4de6deec4f46e279573a1267
DemetriusMitry <54234499+DemetriusMitry@users.noreply.github.com>
2024-10-10 14:09:10 -0300

    Update baixar-revista.bat

--- baixar-revista.bat
+++ baixar-revista.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 REM - essa linha pede o identificador do site
 set /p id="Identificador: "
@@ -6,10 +7,10 @@ set /p id="Identificador: "
 REM - essa linha acessa a pasta Pictures do computador
 cd %USERPROFILE%\Pictures
 
-REM - essa linha cria numero de 1 a 32 acrescentando 1 e a linha wget e onde ira baixar o arquivo
+REM - essa linha cria número de 1 a 32 acrescentando 1 e a linha curl onde irá baixar o arquivo
 for /L %%x in (1,1,32) do (
-
-     curl -O "%%x.jpg" "https://builder.dentrodahistoria.com.br/api/builder/getpreview/%id%?page=%%x&w=1000&v=0"
+     
+	 curl -o "%%x.jpg" "https://builder.dentrodahistoria.com.br/api/builder/getpreview/%id%?page=%%x&w=1000&v=0"
 )
 
 pause
