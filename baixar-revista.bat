76a9c65f50034256b20b030e7bf2d1eb77be7bcb
DemetriusMitry <54234499+DemetriusMitry@users.noreply.github.com>
2024-10-10 14:06:23 -0300

    Update baixar-revista.bat

--- baixar-revista.bat
+++ baixar-revista.bat
@@ -9,7 +9,7 @@ cd %USERPROFILE%\Pictures
 REM - essa linha cria numero de 1 a 32 acrescentando 1 e a linha wget e onde ira baixar o arquivo
 for /L %%x in (1,1,32) do (
 
-    wget -O "%%x.jpg" "https://builder.dentrodahistoria.com.br/api/builder/getpreview/%id%?page=%%x&w=1000&v=0"
+     curl -O "%%x.jpg" "https://builder.dentrodahistoria.com.br/api/builder/getpreview/%id%?page=%%x&w=1000&v=0"
 )
 
 pause
